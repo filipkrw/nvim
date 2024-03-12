794437fe226fd75e13ea7f51e62bcfd1b3decd10
Filip Krawczyk <filip@algomo.com>
2024-03-12 22:43:55 +0000

    Bunch of new configs

--- init.lua
+++ init.lua
@@ -14,18 +14,18 @@ vim.api.nvim_set_keymap('i', '<C-q>', '<space><esc>ce', {
     noremap = true
 })
 
-vim.api.nvim_set_keymap('n', '<leader>j', 'gh', {});
+vim.api.nvim_set_keymap('n', '<leader>f', 'gh', {});
 
 -- Move between tabs and tab groups
-vim.api.nvim_set_keymap('n', 'H', 'gT', {});
-vim.api.nvim_set_keymap('n', 'L', 'gt', {});
-vim.api.nvim_set_keymap('n', 'J', '<C-w>h', {});
-vim.api.nvim_set_keymap('n', 'K', '<C-w>l', {});
+vim.api.nvim_set_keymap('n', '<leader>h', 'gT', {});
+vim.api.nvim_set_keymap('n', '<leader>l', 'gt', {});
+vim.api.nvim_set_keymap('n', 'H', '<C-w>h', {});
+vim.api.nvim_set_keymap('n', 'L', '<C-w>l', {});
 
-vim.api.nvim_set_keymap('v', 'H', 'gT', {});
-vim.api.nvim_set_keymap('v', 'L', 'gt', {});
-vim.api.nvim_set_keymap('v', 'J', '<C-w>h', {});
-vim.api.nvim_set_keymap('v', 'K', '<C-w>l', {});
+vim.api.nvim_set_keymap('v', '<leader>h', 'gT', {});
+vim.api.nvim_set_keymap('v', '<leader>l', 'gt', {});
+vim.api.nvim_set_keymap('v', 'H', '<C-w>h', {});
+vim.api.nvim_set_keymap('v', 'L', '<C-w>l', {});
 
 -- Use system clipboard
 vim.opt.clipboard = "unnamedplus"
@@ -50,32 +50,59 @@ vim.api.nvim_set_keymap('v', '//', 'y/\\V<C-R>=escape(@",\'/\')<CR><CR>', {
     noremap = true
 })
 
--- Clear highlight on esc
-vim.api.nvim_set_keymap('n', '<esc>', ':noh<CR>', {
+-- Search and replace selection
+vim.api.nvim_set_keymap('v', '<leader>r', 'y/\\V<C-R>=escape(@",\'/\')<CR><CR>Ncgn', {
     noremap = true
 })
 
--- Repeat last search remap
-vim.api.nvim_set_keymap('n', '<C-n>', 'N', {
+
+-- Clear highlight on esc
+vim.api.nvim_set_keymap('n', '<esc>', ':noh<CR>', {
     noremap = true,
     silent = true
 })
 
+-- Jump to function body
+vim.api.nvim_set_keymap('n', '<leader>w', '$i<CR><Esc>yyPxa  ', {
+    noremap = true,
+    silent = true
+})
+
+-- Remap J (used for scrolling)
+vim.api.nvim_set_keymap('n', '<leader>j', 'J', {
+  noremap = true,
+  silent = true
+})
+
 -- Scrolling
-vim.api.nvim_set_keymap('n', 'N', '10j', {
-    noremap = true,
-    silent = true
+local move_lines = function(lines, direction)
+    vim.cmd('keepjumps norm! ' .. lines .. direction)
+end
+
+vim.api.nvim_set_keymap('n', 'J', '', {
+  callback = function() move_lines(10, 'j') end,
+  noremap = true,
+  silent = true
 })
-vim.api.nvim_set_keymap('n', 'M', '10k', {
-    noremap = true,
-    silent = true
+vim.api.nvim_set_keymap('n', 'K', '', {
+  callback = function() move_lines(10, 'k') end,
+  noremap = true,
+  silent = true
 })
-vim.api.nvim_set_keymap('v', 'N', '10j', {
-    noremap = true,
-    silent = true
+vim.api.nvim_set_keymap('v', 'J', '', {
+  callback = function() move_lines(10, 'j') end,
+  noremap = true,
+  silent = true
 })
-vim.api.nvim_set_keymap('v', 'M', '10k', {
-    noremap = true,
-    silent = true
+vim.api.nvim_set_keymap('v', 'K', '', {
+  callback = function() move_lines(10, 'k') end,
+  noremap = true,
+  silent = true
+})
+
+-- Delete and edit next search result
+vim.api.nvim_set_keymap('n', '<leader>r', 'cgn', {
+  noremap = true,
+  silent = true
 })
 
